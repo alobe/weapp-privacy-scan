75043f1f7ad2c0a18ade87e7d2eeffa5d6f44af7
alobe <alobef0515@gmail.com>
2023-08-21 17:28:33 +0800

    feat: adjust build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   clean: true,
   noExternal: [/./],
   format: 'cjs',
-  minify: true
+  minify: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
 })
